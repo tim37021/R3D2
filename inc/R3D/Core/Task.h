7f42c5e1326e03cf984fffefe154a28623599e6c
tim37021 <tim37021@gmail.com>
2016-07-14 14:03:56 +0800

    Added QuickTask for lambda writing

--- inc/R3D/Core/Task.h
+++ inc/R3D/Core/Task.h
@@ -2,6 +2,7 @@
 #define _R3D_CORE_TASK_H_
 
 #include <cstdint>
+#include <functional>
 
 namespace r3d
 {
@@ -11,8 +12,25 @@ namespace r3d
         {
         public:
             Task()=default;
-            ~Task()=default;
+            virtual ~Task()=default;
             virtual void execute()=0;
+        };
+
+        class QuickTask: public Task
+        {
+        public:
+            QuickTask(std::function<void()> work)
+                : m_Work(work)
+            {
+
+            }
+            virtual ~QuickTask()=default;
+            virtual void execute()
+            {
+                m_Work();
+            }
+        private:
+            std::function<void()> m_Work;
         };
     }
 }
